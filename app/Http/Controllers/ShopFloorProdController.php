4786c9039080e3c1a1599c2c562cff8e666a1adb
cristianGzo <cristiango371@gmail.com>
2024-11-05 01:12:26 -0600

    calendar

--- app/Http/Controllers/ShopFloorProdController.php
+++ app/Http/Controllers/ShopFloorProdController.php
@@ -37,14 +37,42 @@ class ShopFloorProdController extends Controller
         return response()-> json(["data"=> $result], 200);
     }
     //obtiene total por Dcab, total, etc.
-    public function totales(){
+    public function totales($startDate, $endDate, $startTime = null, $endTime = null){
+        $startDateTime = $startDate . ' ' . ($startTime ?? '00:00:00');
+        $endDateTime = $endDate . ' ' . ($endTime ?? '23:59:59');
+
     $results = Mbctque::selectRaw('SUBSTRING(BUILD_CODE, 1, 2) as Categoria, COUNT(*) as Total')
+        ->whereBetween('CREATE_TS', [$startDateTime, $endDateTime])
         ->groupBy(DB::raw('SUBSTRING(BUILD_CODE, 1, 2)'))
         ->orderBy('Categoria')
         ->get();
 
-    return response()->json(["data"=> $results], 200);
+    return  $results;
     }
+
+    public function validateDate(Request $request)
+    {
+
+        $request->validate([
+            'start_date' => 'required|date',
+            'end_date' => 'required|date',
+            'start_time' => 'nullable|date_format:H:i:s',
+            'end_time' => 'nullable|date_format:H:i:s',
+        ]);
+
+
+        $startDate = $request->input('start_date');
+        $endDate = $request->input('end_date');
+        $startTime = $request->input('start_time');
+        $endTime = $request->input('end_time');
+
+
+        $totals = $this->totales($startDate, $endDate, $startTime, $endTime);
+
+
+        return response()->json(["data"=> $totals], 200);
+    }
+
 
     public function filterInfo($year=null){
         $result = (is_null($year) || $year=="")?
